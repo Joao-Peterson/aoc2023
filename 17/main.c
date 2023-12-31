66922eead5b180eb155e70c6c6e0712f057fcb43
peterson <peterson.sheff.dev@gmail.com>
2023-12-31 16:04:58 -0300

    day 17 complete, finally

--- 17/main.c
+++ 17/main.c
@@ -60,73 +60,27 @@ struct point_t{
 	point_t *next;
 };
 
-typedef struct{
-	point_t *first;
-	point_t *last;
-	size_t size;
-}pqueue_t;
+bool point_cmp(void *a, void *b){
+	point_t *pa = (point_t*)a;
+	point_t *pb = (point_t*)b;
 
-void pqueue_push(pqueue_t *q, int heat, int x, int y, int dirx, int diry, int travel){
-	point_t *p = calloc(1, sizeof(point_t));
-	p->y = y;
-	p->x = x;
-	p->heat = heat;
-	p->next = NULL;
-	p->dirx = dirx;
-	p->diry = diry;
-	p->travel = travel;
-	// p->fromy = fromy;
-	// p->fromx = fromx;
-
-	if(q->size == 0){
-		q->first = p;
-		q->last = p;
-		q->size++;
-	}
-	else{
-		point_t *cursor;
-		for(cursor = q->first; cursor != NULL; cursor = cursor->next){
-			if(p->heat < cursor->heat){
-				q->first = p;
-				p->next = cursor;
-				q->size++;
-				break;
-			}
-			else{
-				if(cursor->next == NULL){
-					q->last = p;
-					cursor->next = p;
-					q->size++;
-					break;	
-				}
-				else if(p->heat < cursor->next->heat){
-					p->next = cursor->next;
-					cursor->next = p;
-					q->size++;
-					break;	
-				}
-
-			}
-		}
-	}
+	return pa->heat < pb->heat;
 }
 
-point_t *pqueue_pop(pqueue_t *q){
-	if(q->size == 0)
-		return NULL;
+void point_push(pqueue_t *pq, int heat, int x, int y, int dx, int dy, int travel){
+	point_t *p = malloc(sizeof(point_t));
+	p->heat = heat;
+	p->x = x;
+	p->y = y;
+	p->dirx = dx;
+	p->diry = dy;
+	p->travel = travel;
 
-	if(q->size == 1){
-		point_t *p = q->first;
-		q->first = NULL;
-		q->last = NULL;
-		q->size = 0;
-		return p;
-	}
+	pqueue_push(pq, p);
+}
 
-	point_t *p = q->first;
-	q->first = p->next;
-	q->size--;
-	return p;
+point_t *point_pop(pqueue_t *pq){
+	return (point_t*)pqueue_pop(pq);
 }
 
 bool inside(int y, int x, const rails_t *r){
@@ -134,9 +88,9 @@ bool inside(int y, int x, const rails_t *r){
 }
 
 uint64_t part1(const rails_t *rails){
-	pqueue_t q = {0};
-	// 141 * 141 area, out of wich, each cell could have different neirbour states of the 4 different directions with 3 different travel distances 
-	// 141*141*4*3 possible states, lets lowball it
+	pqueue_t *q = pqueue_new(point_cmp, true);
+	// 141 * 141 area, out of wich, each cell could have different neirbour states of the 4 different directions with 3 different travel distances that enables 3 adjacent cells to move in each direction, so 4*3 states
+	// 141*141*4*3 maximum possible states
 	set_t *seen = set_new(141*141*4*3);
 	
 	int visited[141][141][2] = {0};
@@ -152,20 +106,21 @@ uint64_t part1(const rails_t *rails){
 	uint64_t min = 0;
 
 	// start
-	pqueue_push(&q, 0, 0, 0, 1, 0, 0);
+	point_push(q, 0, 0, 0, 1, 0, 0);
 
 	// consume less heated points
-	for(point_t *p = pqueue_pop(&q); p != NULL; p = pqueue_pop(&q)){
+	for(point_t *p = point_pop(q); p != NULL; p = point_pop(q)){
 		
 		if(set_exists(seen, p, ((void*)(&p->travel + 1) - (void*)&p->x)))
 			continue;
 
 		set_add(seen, p, ((void*)(&p->travel + 1) - (void*)&p->x));
 
-		visited[p->y][p->x][0] = p->y - p->diry;
-		visited[p->y][p->x][1] = p->x - p->dirx;
-		if(p->heat < losses[p->y][p->x])
+		if(p->heat < losses[p->y][p->x]){
+			visited[p->y][p->x][0] = p->y - p->diry;
+			visited[p->y][p->x][1] = p->x - p->dirx;
 			losses[p->y][p->x] = p->heat;	
+		}
 
 		// end on bottom right
 		if(p->x == rails->w - 1 && p->y == rails->h - 1){
@@ -184,7 +139,7 @@ uint64_t part1(const rails_t *rails){
 		nx = p->x + dx;
 		ny = p->y + dy;
 		if(inside(ny, nx, rails) && p->travel < 3){
-			pqueue_push(&q, 
+			point_push(q, 
 				p->heat + rails->heatLoss[ny][nx],
 				nx, ny,
 				dx, dy,
@@ -198,7 +153,7 @@ uint64_t part1(const rails_t *rails){
 		nx = p->x + dx;
 		ny = p->y + dy;
 		if(inside(ny, nx, rails)){
-			pqueue_push(&q, 
+			point_push(q, 
 				p->heat + rails->heatLoss[ny][nx],
 				nx, ny,
 				dx, dy,
@@ -212,7 +167,7 @@ uint64_t part1(const rails_t *rails){
 		nx = p->x + dx;
 		ny = p->y + dy;
 		if(inside(ny, nx, rails)){
-			pqueue_push(&q, 
+			point_push(q, 
 				p->heat + rails->heatLoss[ny][nx],
 				nx, ny,
 				dx, dy,
@@ -234,33 +189,175 @@ uint64_t part1(const rails_t *rails){
 		printf("\n");
 	}
 
-	// printf("From:\n");
-	// for(int i = 0; i < rails->h; i++){
-	// 	for(int j = 0; j < rails->w; j++){
-	// 		if(j > 0)
-	// 			printf(", ");
+	printf("From:\n");
+	for(int i = 0; i < rails->h; i++){
+		for(int j = 0; j < rails->w; j++){
+			if(j > 0)
+				printf(", ");
 
-	// 		if(visited[i][j][0] == -1 && visited[i][j][1] == -1)
-	// 			printf("[%c,%c]", '#', '#');
-	// 		else
-	// 			printf("[%d,%d]", visited[i][j][0], visited[i][j][1]);
-	// 	}
-	// 	printf("\n");
-	// }
+			if(visited[i][j][0] == -1 && visited[i][j][1] == -1)
+				printf("[%c,%c]", '#', '#');
+			else
+				printf("[%d,%d]", visited[i][j][0], visited[i][j][1]);
+		}
+		printf("\n");
+	}
 
-	// int tx = rails->w - 1, ty = rails->h - 1;
-	// int i = 0;
-	// for(int x = tx, y = ty; x > -1 && y > -1; tx = visited[y][x][0], ty = visited[y][x][1], x = tx, y = ty){
-	// 	rails->path[y][x] = '#';
-	// 	i++;
+	int tx = rails->w - 1, ty = rails->h - 1;
+	int i = 0;
+	for(int x = tx, y = ty; x > -1 && y > -1; tx = visited[y][x][1], ty = visited[y][x][0], x = tx, y = ty){
+		path[y][x] = '#';
+		i++;
 
-	// 	if(i > 1000)
-	// 		break;
-	// }
+		// anti blowup
+		if(i > 1000)
+			break;
+	}
 
-	// printf("Path:\n");
-	// printStringMatrix(rails->path, rails->w, rails->h);
+	printf("Path:\n");
+	for(i = 0; i < rails->h; i++){
+		printf("%.*s\n", (int)rails->w, path[i]);
+	}
 
+	pqueue_destroy(q);
+	set_destroy(seen);
+	return min;
+}
+
+uint64_t part2(const rails_t *rails){
+	pqueue_t *q = pqueue_new(point_cmp, true);
+	// 141 * 141 area, out of wich, each cell could have different neirbour states of the 4 different directions with 3 different travel distances that enables 3 adjacent cells to move in each direction, so 4*3 states
+	// 141*141*4*3 maximum possible states
+	// had to bump set size here, dont know why :P
+	set_t *seen = set_new(141*141*4*10);
+	
+	int visited[141][141][2] = {0};
+	memset(visited, -1, sizeof(int)*141*141*2);
+	int losses[141][141] = {0};
+	for(int i = 0; i < 141; i++)
+		for(int j = 0; j < 141; j++)
+			losses[i][j] = 100000;
+
+	char path[141][141] = {0};
+	memset(path, ' ', sizeof(char)*141*141);
+	
+	uint64_t min = 0;
+
+	// start
+	point_push(q, 0, 0, 0, 1, 0, 0);
+
+	// consume less heated points
+	for(point_t *p = point_pop(q); p != NULL; p = point_pop(q)){
+		
+		if(set_exists(seen, p, ((void*)(&p->travel + 1) - (void*)&p->x)))
+			continue;
+
+		set_add(seen, p, ((void*)(&p->travel + 1) - (void*)&p->x));
+
+		if(p->heat < losses[p->y][p->x]){
+			visited[p->y][p->x][0] = p->y - p->diry;
+			visited[p->y][p->x][1] = p->x - p->dirx;
+			losses[p->y][p->x] = p->heat;	
+		}
+
+		// end on bottom right
+		if(p->x == rails->w - 1 && p->y == rails->h - 1){
+			min = p->heat; 
+			break;
+		}
+
+		int nx;
+		int ny;
+		int dx;
+		int dy;
+
+		// ahead
+		dx = p->dirx;
+		dy = p->diry;
+		nx = p->x + dx;
+		ny = p->y + dy;
+		if(inside(ny, nx, rails) && p->travel < 10){
+			point_push(q, 
+				p->heat + rails->heatLoss[ny][nx],
+				nx, ny,
+				dx, dy,
+				p->travel + 1
+			);		
+		}
+
+		// adjacent side
+		dx = p->diry;
+		dy = p->dirx;
+		nx = p->x + dx;
+		ny = p->y + dy;
+		if(inside(ny, nx, rails) && (p->travel >= 4 || (p->x == 0 && p->y == 0))){
+			point_push(q, 
+				p->heat + rails->heatLoss[ny][nx],
+				nx, ny,
+				dx, dy,
+				1
+			);		
+		}
+
+		// other adjacent side
+		dx = - p->diry;
+		dy = - p->dirx;
+		nx = p->x + dx;
+		ny = p->y + dy;
+		if(inside(ny, nx, rails) && (p->travel >= 4 || (p->x == 0 && p->y == 0))){
+			point_push(q, 
+				p->heat + rails->heatLoss[ny][nx],
+				nx, ny,
+				dx, dy,
+				1
+			);		
+		}
+	}
+
+	printf("Heatloss:\n");
+	printStringNumMatrix(rails->heatLoss, rails->w, rails->h, false);
+	printf("Losses:\n");
+	for(int i = 0; i < rails->h; i++){
+		for(int j = 0; j < rails->w; j++){
+			if(j > 0)
+				printf(", ");
+
+			printf("%d", losses[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("From:\n");
+	for(int i = 0; i < rails->h; i++){
+		for(int j = 0; j < rails->w; j++){
+			if(j > 0)
+				printf(", ");
+
+			if(visited[i][j][0] == -1 && visited[i][j][1] == -1)
+				printf("[%c,%c]", '#', '#');
+			else
+				printf("[%d,%d]", visited[i][j][0], visited[i][j][1]);
+		}
+		printf("\n");
+	}
+
+	int tx = rails->w - 1, ty = rails->h - 1;
+	int i = 0;
+	for(int x = tx, y = ty; x > -1 && y > -1; tx = visited[y][x][1], ty = visited[y][x][0], x = tx, y = ty){
+		path[y][x] = '#';
+		i++;
+
+		// anti blowup
+		if(i > 1000)
+			break;
+	}
+
+	printf("Path:\n");
+	for(i = 0; i < rails->h; i++){
+		printf("%.*s\n", (int)rails->w, path[i]);
+	}
+
+	pqueue_destroy(q);
 	set_destroy(seen);
 	return min;
 }
@@ -299,9 +396,7 @@ int main(int argc, char**argv){
 
 	rails_t *rails = parseInput(data);
 	printf("Part 1: %lu\n", part1(rails));
-	// too high 1291
-	
-	// printf("Part 2: %lu\n", );
+	printf("Part 2: %lu\n", part2(rails));
 
 	free(rails); 
 	string_destroy(data);
